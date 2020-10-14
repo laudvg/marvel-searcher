359f5410ebf76c4bedf312aff2ec170133330e2c
laudvg <laura.vivas.gamboa@gmail.com>
2020-10-14 11:02:20 +0200

    first commit, started project

---
+++ src/list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './list.css';
+
+class List extends Component {
+  render() {
+    const {thumbnail} = spidey.data.results[0];
+    return (
+      
+      <div>
+
+      </div>
+      
+    );
+
+  }
+
+}
