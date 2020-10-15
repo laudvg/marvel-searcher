6dbb24aed4cbcac0d03f144a8664a549f38b7a92
laudvg <laura.vivas.gamboa@gmail.com>
2020-10-15 11:10:11 +0200

    added modal with descriptions

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -1,12 +1,51 @@
-import React  from 'react';
+import React , { useState } from 'react';
+import Modal from "react-modal";
 import './list-item.css';
 
-const ListItem = ({name, thumbnail, description}) => (
-  <div className="superheroe">
+
+Modal.setAppElement("#root")
+
+function ListItem  ({name, thumbnail, description}){
+
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  
+  return(
+    <div className="superheroe">
     <h3 className="name">{name}</h3>
     <img className="thumbnail" src={`${thumbnail.path}.${thumbnail.extension}`} alt="superheroe"/>
-    <p className="description">{description}</p>
+    <button onClick={() => setModalIsOpen(true)}>About</button>
+
+      <Modal 
+      isOpen ={modalIsOpen} 
+      onRequestClose ={() => setModalIsOpen(false)}
+
+      style = {
+        { 
+          overlay:{
+            backgroundColor: "#000000"
+          }
+        },
+        {
+          content : {
+            top                   : '50%',
+            left                  : '50%',
+            right                 : 'auto',
+            bottom                : 'auto',
+            marginRight           : '-50%',
+            marginTop             : '2%',
+            transform             : 'translate(-50%, -50%)'
+          }
+        }
+      }
+      >
+        <button onClick={() => setModalIsOpen(false)}>X</button>
+        <h3 className="name">{name}</h3>
+        <img className="thumbnail" src={`${thumbnail.path}.${thumbnail.extension}`} alt="superheroe"/>
+        <p className="description">{description}</p>
+      </Modal>
   </div>
-)
+  )
+ }
+  
 
 export default ListItem;
