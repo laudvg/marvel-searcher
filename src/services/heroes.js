47b317c96ae07bc176a35a6d212aeed26fcfb094
laudvg <laura.vivas.gamboa@gmail.com>
2020-10-15 14:29:12 +0200

    cleaned url

--- src/services/heroes.js
+++ src/services/heroes.js
@@ -1,16 +1,16 @@
 const apiKey = process.env.REACT_APP_MARVEL_API_KEY;
 const hash = process.env.REACT_APP_MARVEL_HASH;
-// const basic = 
+const baseUrl = "http://gateway.marvel.com/v1/public/characters?"
 
-export async function getHeroes(limit = 12){
-  const response = await fetch(`http://gateway.marvel.com/v1/public/characters?limit=${limit}&ts=1&apikey=${apiKey}&hash=${hash}`)
+export async function getHeroes(limit = 20){
+  const response = await fetch(`${baseUrl}limit=${limit}&ts=1&apikey=${apiKey}&hash=${hash}`)
   const responseJson = await response.json()
 
   return responseJson.data.results;
 }
 
-export async function getBySearch(query, limit = 12){
-  const response = await fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&limit=${limit}&ts=1&apikey=${apiKey}&hash=${hash}`)
+export async function getBySearch(query, limit = 20){
+  const response = await fetch(`${baseUrl}nameStartsWith=${query}&limit=${limit}&ts=1&apikey=${apiKey}&hash=${hash}`)
   const responseJson = await response.json()
 
   return responseJson.data.results;
