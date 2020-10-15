fa6945522c580f42f915509182b17696f883c895
laudvg <laura.vivas.gamboa@gmail.com>
2020-10-15 04:16:32 +0200

    created new component with the marvel logo on it

---
+++ src/components/marvel-fixed.js
@@ -0,0 +1,11 @@
+import React  from 'react';
+import '../App.css';
+
+const MarvelBar = ({name, thumbnail, description}) => (
+  <div className="marvel-bar">
+        <img className="marvel-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/MarvelLogo.svg/1200px-MarvelLogo.svg.png" alt="Marvel"/>
+        <h1 className="marvel-search">SEARCH</h1>
+  </div>
+)
+
+export default MarvelBar;
