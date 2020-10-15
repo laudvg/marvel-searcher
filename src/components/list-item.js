9d1994dcb79cb947b3ee97b14a9de9402a0c5da3
laudvg <laura.vivas.gamboa@gmail.com>
2020-10-15 04:15:21 +0200

    changed styles

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -9,18 +9,4 @@ const ListItem = ({name, thumbnail, description}) => (
   </div>
 )
 
-// class ListItem extends Component {
-//   render() {
-//     const data = this.props.superheroe
-//     const {thumbnail, name, description} = data;
-//     return (
-//       <div className="superheroe">
-//             <img className="thumbnail" src={`${thumbnail.path}.${thumbnail.extension}`} alt="superheroe"/>
-//             <h2 className="name">{name}</h2>
-//             <p className="description">{description}</p>
-//       </div>  
-//     );
-//   }
-// }
-
 export default ListItem;
