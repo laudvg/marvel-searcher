2712d1c647c6460dde35729b8d18a15f07e3e284
laudvg <laura.vivas.gamboa@gmail.com>
2020-10-15 11:31:07 +0200

    Modal styles done

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -1,6 +1,7 @@
 import React , { useState } from 'react';
 import Modal from "react-modal";
 import './list-item.css';
+import '../App.css';
 
 
 Modal.setAppElement("#root")
@@ -18,25 +19,25 @@ function ListItem  ({name, thumbnail, description}){
       <Modal 
       isOpen ={modalIsOpen} 
       onRequestClose ={() => setModalIsOpen(false)}
-
-      style = {
-        { 
-          overlay:{
-            backgroundColor: "#000000"
-          }
-        },
-        {
-          content : {
-            top                   : '50%',
-            left                  : '50%',
-            right                 : 'auto',
-            bottom                : 'auto',
-            marginRight           : '-50%',
-            marginTop             : '2%',
-            transform             : 'translate(-50%, -50%)'
-          }
-        }
-      }
+      className="Modal"
+      // style = {
+      //   { 
+      //     overlay:{
+      //       backgroundColor: "#000000"
+      //     }
+      //   },
+      //   {
+      //     content : {
+      //       top                   : '50%',
+      //       left                  : '50%',
+      //       right                 : 'auto',
+      //       bottom                : 'auto',
+      //       marginRight           : '-50%',
+      //       marginTop             : '2%',
+      //       transform             : 'translate(-50%, -50%)'
+      //     }
+      //   }
+      // }
       >
         <button onClick={() => setModalIsOpen(false)}>X</button>
         <h3 className="name">{name}</h3>
