f685e692ce8b2ec95f606b083bc33c2afb4baf4d
laudvg <laura.vivas.gamboa@gmail.com>
2020-10-15 04:00:49 +0200

    made async function get by search

--- src/services/heroes.js
+++ src/services/heroes.js
@@ -1,19 +1,22 @@
-export async function getHeroes(){
-  console.log(process.env)
+const apiKey = process.env.REACT_APP_MARVEL_API_KEY;
+const hash = process.env.REACT_APP_MARVEL_HASH;
+// const basic = 
 
-  const apiKey = process.env.REACT_APP_MARVEL_API_KEY;
-  const hash = process.env.REACT_APP_MARVEL_HASH;
-
-  const response = await fetch(`http://gateway.marvel.com/v1/public/characters?limit=12&ts=1&apikey=${apiKey}&hash=${hash}`)
+export async function getHeroes(limit = 12){
+  const response = await fetch(`http://gateway.marvel.com/v1/public/characters?limit=${limit}&ts=1&apikey=${apiKey}&hash=${hash}`)
   const responseJson = await response.json()
 
-  return responseJson
-  
-  // .then(response => response.json())
-  // .then(heroeJson => this.setState({heroes:heroeJson.data.results, isFetch: false}))
+  return responseJson.data.results;
+}
 
+export async function getBySearch(query, limit = 12){
+  const response = await fetch(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&limit=${limit}&ts=1&apikey=${apiKey}&hash=${hash}`)
+  const responseJson = await response.json()
+
+  return responseJson.data.results;
 }
 
 export default {
-  getHeroes
+  getHeroes,
+  getBySearch
 }
